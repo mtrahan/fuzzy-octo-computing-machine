6f027f79f6c546e01fb9b99a50d60e5ae2d59bec
Ubuntu <ubuntu@ip-172-31-32-16.ec2.internal>
2014-07-28 19:04:06 +0000

    setup script

---
+++ setup-instance.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt-get install -y nodejs npm
+sudo apt-get install -y postgresql-client
+sudo apt-get install -y libpq-dev
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y ruby-dev
+sudo apt-get install -y ruby
+sudo gem install sqlite3-ruby
+sudo gem install rails
